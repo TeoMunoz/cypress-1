0845680fc0de637f1b7c6b5ae65570fd0ac88848
TeoMunoz <teo.munoz.blaso@gmail.com>
2022-12-20 11:35:08 +0100

    The basic functions of the web.

--- webapp/index.php
+++ webapp/index.php
@@ -1,4 +1,63 @@
 <?php $page_title = "Home ★ Productive"; ?>
 <?php require "view/blocks/page_start.php"; ?>
-<h1>Welcome to Productive!</h1>
 <?php require "view/blocks/page_end.php"; ?>
+<h1>Welcome to Productive!</h1>
+
+<h2>Our most popular products</h2>
+
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Price</th>
+      <th>Stock</th>
+    </tr>
+  </thead>
+  <tbody id="products-table">
+  </tbody>
+</table>
+
+<script>
+const updateProductTable = async function (table, productsToInsert) {
+  const columns = ['name', 'price', 'stock'];
+
+  // Insert the table rows for the found products
+  const newTableRows = productsToInsert.map((product) => {
+    let stock = document.createElement('tr');
+
+    const tds = [];
+
+    for (let i = 0; i < columns.length; i++) {
+      let column = columns[i];
+
+      //Stock filter
+      let td = document.createElement('td');
+      if(column === 'stock') {
+        if (product[column] <= 3) {
+          td.style.color = 'red'
+        } else {
+          td.style.color = 'black';
+        }
+      }
+      td.innerText = product[column];
+      stock.appendChild(td);
+    }
+
+    return stock;
+  })
+
+  table.replaceChildren(...newTableRows)
+}
+
+const init = async function () { // an async wrapper function which allows me to use await instead of .then()
+  const result = await fetch('API/V1/Products')
+  const productsResult = await result.json()
+  const products = productsResult.filter((product) => product.active === "1")
+
+  console.log(products)
+
+  updateProductTable(document.querySelector('#products-table'), products);
+}
+
+init();
+</script>
